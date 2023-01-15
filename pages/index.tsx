16fa3f6c1e9e70f5f353e4b4c41e252abfea174a
Jonathan Cai <caimjonathan@gmail.com>
2023-01-15 00:40:09 -0500

    fix: github linnk

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
           </p>
           <a
             className="mt-4 flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800"
-            href="https://github.com/steven-tey/precedent"
+            href="https://github.com/shrimpynuts/search-ens"
             target="_blank"
             rel="noopener noreferrer"
           >
