78215026edc118b2fed80be200383a787571de31
Jonathan Cai <caimjonathan@gmail.com>
2023-01-15 00:00:25 -0500

    feat: implement hot query on text update

--- components/ens/search.tsx
+++ components/ens/search.tsx
@@ -1,52 +1,70 @@
-import { ReactNode, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useProvider } from "wagmi";
 
-export default function Search({ query }: { query?: ReactNode }) {
+const parseTextArea = (text: string) => {
+  const lines = text.split("\n");
+  const noWhitespaceLines = lines.map((line) => line.replace(/\s/g, ""));
+  const dotEthAppended = noWhitespaceLines.map((line) =>
+    line.length > 3 ? `${line}.eth` : "",
+  );
+  return dotEthAppended;
+};
+
+export default function Search() {
   const provider = useProvider();
   const [textArea, setTextArea] = useState<string>("vitalik");
   const [queriedNames, setQueriedNames] = useState<{ [name: string]: string }>(
     {},
   );
-
-  const parseTextArea = (text: string) => {
-    const lines = text.split("\n");
-    const noWhitespaceLines = lines.map((line) => line.replace(/\s/g, ""));
-    const dotEthAppended = noWhitespaceLines.map((line) => `${line}.eth`);
-    return dotEthAppended;
-  };
   const queryNames = parseTextArea(textArea);
+  const [isTyping, setIsTyping] = useState(false);
+
+  useEffect(() => {
+    if (isTyping) {
+      const timer = setTimeout(() => {
+        if (isTyping) {
+          setIsTyping(false);
+          if (!provider) return console.error("Could not get provider!");
+          resolveNames(queryNames);
+        }
+      }, 500);
+      console.log({ timer });
+    }
+  }, [isTyping]);
 
   const resolveNames = async (names: string[]) => {
+    const namesToFetch = names.filter((name) => !(name in queriedNames));
+    console.log(`Fetching ${namesToFetch.length} names!`);
     return await Promise.all(
-      names
-        .filter((name) => !(name in queriedNames))
-        .map((name) =>
-          provider.resolveName(name).then((result) => {
-            setQueriedNames((prev) => {
-              const status = result ? "Unavailable" : "Available";
-              const obj = { ...prev, [name]: status };
-              return obj;
-            });
-          }),
-        ),
+      namesToFetch.map((name) =>
+        provider.resolveName(name).then((result) => {
+          setQueriedNames((prev) => {
+            const status = result ? "Unavailable" : "Available";
+            const obj = { ...prev, [name]: status };
+            return obj;
+          });
+        }),
+      ),
     );
   };
 
-  const fetchNames = () => {
-    if (!provider) return console.error("Could not get provider!");
-    resolveNames(queryNames);
+  const onTextAreaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setTextArea(event.target.value);
+    if (!isTyping) {
+      setIsTyping(true);
+    }
   };
 
   return (
     <div className="relative w-full rounded-xl border border-gray-200 bg-white p-8 shadow-md">
-      <button onClick={fetchNames}>Fetch Names</button>
+      {/* <button onClick={fetchNames}>Fetch Names</button> */}
       <div className="grid grid-cols-5">
         <textarea
           className="col-span-3 whitespace-nowrap	"
-          rows={8}
+          rows={10}
           cols={50}
           value={textArea}
-          onChange={(event) => setTextArea(event.target.value)}
+          onChange={onTextAreaChange}
         ></textarea>
         <div className="pt-2 pl-2">
           {queryNames.map((name, idx) => (
@@ -54,9 +72,25 @@ export default function Search({ query }: { query?: ReactNode }) {
           ))}
         </div>
         <div className="pt-2 pl-2">
-          {queryNames.map((name, idx) => (
-            <p key={idx}>{queriedNames[name]}</p>
-          ))}
+          {queryNames.map((name, idx) => {
+            const ensLink = `https://app.ens.domains/name/${name}/details`;
+            return (
+              <p key={idx}>
+                <a
+                  className={`${
+                    queriedNames[name] === "Available"
+                      ? "text-green-500"
+                      : "text-red-500"
+                  }`}
+                  href={ensLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {queriedNames[name]}
+                </a>
+              </p>
+            );
+          })}
         </div>
       </div>
     </div>
