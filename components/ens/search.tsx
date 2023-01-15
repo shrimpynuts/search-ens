71b81911b64fdf12b426de7394dd46aa7a7f14fb
Jonathan Cai <caimjonathan@gmail.com>
2023-01-14 23:20:05 -0500

    feat: implement basic textarea component

--- components/ens/search.tsx
+++ components/ens/search.tsx
@@ -3,35 +3,62 @@ import { useProvider } from "wagmi";
 
 export default function Search({ query }: { query?: ReactNode }) {
   const provider = useProvider();
-  const [resolvedNames, setResolvedNames] = useState<(string | null)[]>([]);
-  const [queryNames, setQueryNames] = useState([
-    "vitalik.eth",
-    "jonathancai.eth",
-    "sdfeeee.eth",
-  ]);
+  const [textArea, setTextArea] = useState<string>("vitalik");
+  const [queriedNames, setQueriedNames] = useState<{ [name: string]: string }>(
+    {},
+  );
+
+  const parseTextArea = (text: string) => {
+    const lines = text.split("\n");
+    const noWhitespaceLines = lines.map((line) => line.replace(/\s/g, ""));
+    const dotEthAppended = noWhitespaceLines.map((line) => `${line}.eth`);
+    return dotEthAppended;
+  };
+  const queryNames = parseTextArea(textArea);
 
   const resolveNames = async (names: string[]) => {
-    return await Promise.all(names.map((name) => provider.resolveName(name)));
+    return await Promise.all(
+      names
+        .filter((name) => !(name in queriedNames))
+        .map((name) =>
+          provider.resolveName(name).then((result) => {
+            setQueriedNames((prev) => {
+              const status = result ? "Unavailable" : "Available";
+              const obj = { ...prev, [name]: status };
+              return obj;
+            });
+          }),
+        ),
+    );
   };
 
   const fetchNames = () => {
     if (!provider) return console.error("Could not get provider!");
-    resolveNames(queryNames).then(setResolvedNames);
+    resolveNames(queryNames);
   };
 
   return (
-    <div className="relative h-96 rounded-xl border border-gray-200 bg-white p-8 shadow-md">
+    <div className="relative w-full rounded-xl border border-gray-200 bg-white p-8 shadow-md">
       <button onClick={fetchNames}>Fetch Names</button>
-      {queryNames.map((query, idx) => (
-        <div className="grid grid-cols-3" key={idx}>
-          <div className="col-span-2">
-            {idx}: {query}
-          </div>
-          <div className="col-span-1">
-            {resolvedNames[idx] !== null ? resolvedNames[idx]?.slice(0, 8) : ""}
-          </div>
+      <div className="grid grid-cols-5">
+        <textarea
+          className="col-span-3 whitespace-nowrap	"
+          rows={8}
+          cols={50}
+          value={textArea}
+          onChange={(event) => setTextArea(event.target.value)}
+        ></textarea>
+        <div className="pt-2 pl-2">
+          {queryNames.map((name, idx) => (
+            <p key={idx}>{name}</p>
+          ))}
         </div>
-      ))}
+        <div className="pt-2 pl-2">
+          {queryNames.map((name, idx) => (
+            <p key={idx}>{queriedNames[name]}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Layout>
       <motion.div
-        className="max-w-xl px-5 xl:px-0"
+        className="max-w-4xl px-5 xl:px-0"
         initial="hidden"
         whileInView="show"
         animate="show"
@@ -24,27 +24,7 @@ export default function Home() {
           },
         }}
       >
-        {/* <motion.a
-          variants={FADE_DOWN_ANIMATION_VARIANTS}
-          href="https://twitter.com/steventey/status/1613928948915920896"
-          target="_blank"
-          rel="noreferrer"
-          className="mx-auto mb-5 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
-        >
-          <Twitter className="h-5 w-5 text-[#1d9bf0]" />
-          <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing Precedent
-          </p>
-        </motion.a>
-        <motion.h1
-          className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]"
-          variants={FADE_DOWN_ANIMATION_VARIANTS}
-        >
-          <Balancer>ENS Search</Balancer>
-        </motion.h1> */}
-        <div>
-          <Search />
-        </div>
+        <Search />
       </motion.div>
     </Layout>
   );
